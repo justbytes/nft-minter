5810d2faaad822cfffbce39ffe255152ec65c93e
Jesse Marino <100870863+justbytes@users.noreply.github.com>
2023-03-02 20:40:09 -0800

    DesertSlugs smart contract compiled and configured to deploy to goerli test network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_PRIVATE_KEY}`,
+      accounts: [process.env.GOERLI_WALLET_PRIVATE_KEY],
+    },
+  },
 };
